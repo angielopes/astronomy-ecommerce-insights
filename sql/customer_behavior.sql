375de2a4ca4bbb42eae54281f731dd61a8cf65b4
Angie <i4ngela@gmail.com>
2025-06-05 14:18:51 -0300

    feat(sql): add queries for top clients by spending and purchase count

--- sql/customer_behavior.sql
+++ sql/customer_behavior.sql
@@ -21,11 +21,32 @@ valor_liquido_vendas AS (
                 v.total_venda AS total_venda_bruta,
                 COALESCE(vida.total_valor_devolvido_aprovado, 0) AS total_devolvido_venda,
                 CASE
-                        WHEN v.status_venda = "cancelada" THEN 0 -- Vendas canceladas não entrarão em valores líquidos
+                        WHEN v.status_venda = 'cancelada' THEN 0 -- Vendas canceladas não entrarão em valores líquidos
                         ELSE(v.total_venda - COALESCE(vida.total_valor_devolvido_aprovado, 0))
                 END AS total_venda_liquida
         FROM vendas v
         LEFT JOIN valor_itens_devolvidos_aprovados_por_venda vida ON v.id_venda = vida.id_venda
 )
 
-SELECT * FROM valor_liquido_vendas;
+-- Top 10 clientes por valor gasto
+SELECT 
+        vlv.id_cliente,
+        c.nome_cliente,
+        SUM(vlv.total_venda_liquida) AS total_liquido_gasto_cliente
+FROM valor_liquido_vendas vlv
+JOIN clientes c ON vlv.id_cliente = c.id_cliente
+GROUP BY vlv.id_cliente, c.nome_cliente
+ORDER BY total_liquido_gasto_cliente DESC
+LIMIT 10;
+
+-- Top 10 clientes por compras efetivadas
+SELECT
+        v.id_cliente,
+        c.nome_cliente,
+        COUNT(DISTINCT id_venda) AS num_compras_validas_cliente
+FROM vendas v
+JOIN clientes c ON v.id_cliente = c.id_cliente
+WHERE status_venda IN ('concluida', 'devolvida parcialmente')
+GROUP BY id_cliente
+ORDER BY num_compras_validas_cliente DESC
+LIMIT 10;
